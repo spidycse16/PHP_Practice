e39577a030933fa89dc34304b2c495419d4fec6b
spidycse16 <spidy0709@gmail.com>
2024-05-05 00:30:46 +0600

    callback functions

---
+++ callbackFunction.php
@@ -0,0 +1,36 @@
+<?php
+function hey()
+{
+    echo "This is hey function";
+}
+function hello()
+{
+    echo " this is hello function";
+}
+function callback($hi)
+{
+    $hi;
+}
+callback(hey());
+
+
+function exclaim($str)
+{
+    echo $str;
+    return "!";
+}
+
+function ask($str)
+{
+    echo $str;
+    return "?";
+}
+
+function callback2($str,$func)
+{
+    echo $func($str);
+}
+callback2("Hello world","exclaim");
+echo "<br>";
+callback2("HEllo world","ask");
+?>
