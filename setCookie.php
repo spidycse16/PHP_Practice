19c7b32011f2479e43788b13cfdff408ed659776
spidycse16 <spidy0709@gmail.com>
2024-05-04 16:50:04 +0600

    updates

--- setCookie.php
+++ setCookie.php
@@ -1,5 +1,14 @@
 <?php
 //sets the cookie,cookie name,cookie value,time() return the time from 1970 till now, "/" says that this cookie can be used anywhere in the website;
 setcookie("catagory" , "Books", time()+86400,"/");
-echo "cookies are set";
+if(!isset($_COOKIE["catagory"]))
+{
+    echo "cookie is not set yet";
+}
+else
+{
+    $cok=$_COOKIE['catagory'];
+echo "this is $cok";
+}
+
 ?>
